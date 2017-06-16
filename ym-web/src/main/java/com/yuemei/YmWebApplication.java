c27ede288bb7f0c46930d0dcbbff4fa77c75b98e
pengweiqiang <weiqiang.peng@futengwl.com>
2017-06-16 11:13:24 +0800

    dfde

---
+++ ym-web/src/main/java/com/yuemei/YmWebApplication.java
@@ -0,0 +1,12 @@
+package com.yuemei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YmWebApplication.class, args);
+	}
+}
