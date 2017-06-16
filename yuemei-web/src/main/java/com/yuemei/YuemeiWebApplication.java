91e4854de7b6b8bd03120a1e34ca546e9ae62af0
platform-weiqiang <1160100308@qq.com>
2017-06-17 01:04:10 +0800

    update web model

---
+++ yuemei-web/src/main/java/com/yuemei/YuemeiWebApplication.java
@@ -0,0 +1,12 @@
+package com.yuemei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuemeiWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuemeiWebApplication.class, args);
+	}
+}
