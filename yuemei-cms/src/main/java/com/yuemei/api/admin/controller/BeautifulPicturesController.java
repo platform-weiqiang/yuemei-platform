8925077bc12ffeb2436b0719a3fc55903d5f30d8
paltform-weiqiang <1160100308@qq.com>
2017-06-26 18:18:17 +0800

    update page

--- yuemei-cms/src/main/java/com/yuemei/api/admin/controller/BeautifulPicturesController.java
+++ yuemei-cms/src/main/java/com/yuemei/api/admin/controller/BeautifulPicturesController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.yuemei.api.admin.entity.BeautifulPictures;
@@ -23,7 +25,7 @@ public class BeautifulPicturesController {
 	@RequestMapping("/index")
 	@ResponseBody
 	public PageInfo<BeautifulPictures> index(@RequestParam(required = true, defaultValue = "1") Integer pageNum){
-		PageHelper.startPage(pageNum, 10);// 默认从第一页开始，每页五条
+		PageHelper.startPage(pageNum, 9);// 默认从第一页开始，每页五条
 		List<BeautifulPictures> list=beautifulPicturesService.queryList();
 		PageInfo<BeautifulPictures> pageUser = new PageInfo<BeautifulPictures>(list);// 将users对象绑定到pageInfo
 		return pageUser;
@@ -33,7 +35,8 @@ public class BeautifulPicturesController {
 	public String login(@RequestParam(required = true, defaultValue = "1") Integer pageNum,ModelMap map){
 		PageHelper.startPage(pageNum, 9);// 默认从第一页开始，每页五条
 		List<BeautifulPictures> list=beautifulPicturesService.queryList();
-		map.addAttribute("pic", list);
+		PageInfo<BeautifulPictures> pageUser = new PageInfo<BeautifulPictures>(list);// 将users对象绑定到pageInfo
+		map.addAttribute("pic", JSON.toJSON(pageUser));
 		return "index";
 	}
 
