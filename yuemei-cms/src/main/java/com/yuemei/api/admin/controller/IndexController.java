68bbe7090fc4da64f2746fe751aca682d0947c1d
paltform-weiqiang <1160100308@qq.com>
2017-06-27 15:13:45 +0800

    login page

--- yuemei-cms/src/main/java/com/yuemei/api/admin/controller/IndexController.java
+++ yuemei-cms/src/main/java/com/yuemei/api/admin/controller/IndexController.java
@@ -1,7 +1,13 @@
 package com.yuemei.api.admin.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import com.yuemei.api.common.vcode.Captcha;
+import com.yuemei.api.common.vcode.GifCaptcha;
 
 @Controller
 public class IndexController {
@@ -9,5 +15,38 @@ public class IndexController {
 	@RequestMapping("/login")
 	public String login(){
 		return "login";
+	}
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * 获取验证码（Gif版本）
+	 * @param response
+	 * @param request
+	 */
+	@RequestMapping(value="/getGifCode",method=RequestMethod.GET)
+	public void getGifCode(HttpServletResponse response,HttpServletRequest request){
+		try {
+			response.setHeader("Pragma", "No-cache");  
+	        response.setHeader("Cache-Control", "no-cache");  
+	        response.setDateHeader("Expires", 0);  
+	        response.setContentType("image/gif");  
+	        /**
+	         * gif格式动画验证码
+	         * 宽，高，位数。
+	         */
+	        Captcha captcha = new GifCaptcha(116,34,4);
+	        //输出
+	        captcha.out(response.getOutputStream());
+	        HttpSession session = request.getSession(true);
+	      //存入Session
+	        session.setAttribute("_code",captcha.text().toLowerCase());
+		} catch (Exception e) {
+			System.err.println("获取验证码异常："+e.getMessage());
+		}
 	}
 }
