0cae0a10fc8486639b37d7c12a0a5b4475ca1422
paltform-weiqiang <1160100308@qq.com>
2017-07-12 17:54:37 +0800

    dff

---
+++ yueMei-eureka-server/src/main/java/com/yuemei/server/YueMeiEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.yuemei.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YueMeiEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YueMeiEurekaServerApplication.class, args);
+	}
+}
